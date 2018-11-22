c5fa7131ded39cbd405a269aefb34dff292a485a
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-11-22 17:38:49 +0900

    pass to child

--- src/components/MyCompChild.vue
+++ src/components/MyCompChild.vue
@@ -1,3 +1,12 @@
 <template>
-    <div class="comp-child">My Component Child</div>
+  <div class="comp-child">
+    My Component Child
+    <p>{{ val }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["val"]
+};
+</script>
