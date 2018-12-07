a740299181b32f28eefcb014b6bb477c4f8d552b
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-12-07 22:40:09 +0900

    chap8 商品一覧を追加

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="product">
+    <p>このページはID.{{ id }}の詳細です。</p>
     <h1>商品情報</h1>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    id: Number
+  }
+};
+</script>
